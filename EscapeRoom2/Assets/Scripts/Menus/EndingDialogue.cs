88dab540fcd69bbee269d809704c2b41b59fef2a
MauricioMirandaDev <32001207+MauricioMirandaDev@users.noreply.github.com>
2021-10-09 19:44:57 -0700

    Endings Implemented
    Different endings based on whether the player made it to the end or not have been implemented.

---
+++ EscapeRoom2/Assets/Scripts/Menus/EndingDialogue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndingDialogue : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text dialogueBox;
+
+    private AudioSource audioSource;
+
+    private Animator animator;
+
+    [SerializeField]
+    private CountdownTimer countdown;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        countdown = GameObject.Find("Countdown").GetComponent<CountdownTimer>();
+
+        animator.SetBool("PlayerWon", countdown.playerWon);
+    }
+
+    public void PlayDialogue(AudioClip voice)
+    {
+        audioSource.PlayOneShot(voice);
+    }
+
+    public void ChangeText(string text)
+    {
+        dialogueBox.SetText(text);
+    }
+
+    public void SetEnding()
+    {
+        animator.SetBool("PlayerWon", countdown.playerWon);
+    }
+}
